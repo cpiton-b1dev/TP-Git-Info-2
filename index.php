4766713c61ac1cb0cfa010a09bac86207bdf627c
cpiton-b1dev <cpiton.b1dev@gmail.com>
2025-01-23 10:20:58 +0100

    ajout index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="login.css">
+    <title>Accueil - Titre modifié</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main>
+        <div class="px-4 py-5 my-5 text-center">
+            <h1 class="display-5 fw-bold text-body-emphasis">Bienvenue</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Nouveau paragraphe - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
+                    lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+                <div>
+                    <a href="login.php" class="btn btn-outline-primary btn-lg px-4 active">Page de connexion</a>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
