c0f0657ce6b0733c6acfa77e22f60b1540b14137
cpiton-b1dev <cpiton.b1dev@gmail.com>
2025-01-23 11:40:23 +0100

    modification index et ajout login

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="login.css">
-    <title>Accueil - Titre initial</title>
+    <title>Accueil - Titre avec modification</title>
 </head>
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main>
